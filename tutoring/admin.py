4eb91471ff51ba1971a994fb8e0d1a4adc3816d1
Bryan Ngo <bngo92@gmail.com>
2013-10-02 14:39:45 -0700

    Registered foreign tutoring admin

--- tutoring/admin.py
+++ tutoring/admin.py
@@ -25,6 +25,12 @@ class TutoringAdmin(admin.ModelAdmin):
             tutoring.save()
 
 
+class ForeignTutoringAdmin(admin.ModelAdmin):
+    fields = ('day_1', 'hour_1', 'day_2', 'hour_2')
+    list_display = ('__unicode__', 'day_1', 'hour_1', 'day_2', 'hour_2')
+    list_editable = ('day_1', 'hour_1', 'day_2', 'hour_2')
+
+
 class WeekAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'term', 'day_1', 'hour_1', 'day_2', 'hour_2', 'hours', 'tutees')
     list_editable = ('hours', 'tutees')
@@ -35,6 +41,7 @@ class WeekAdmin(admin.ModelAdmin):
 
 admin.site.register(Class, ClassAdmin)
 admin.site.register(Tutoring, TutoringAdmin)
+admin.site.register(ForeignTutoring, ForeignTutoringAdmin)
 admin.site.register(Week3, WeekAdmin)
 admin.site.register(Week4, WeekAdmin)
 admin.site.register(Week5, WeekAdmin)
