e682db3de59e74f663b1a31082a742d5660f1d5f
Bryan Ngo <achow.tk@gmail.com>
2013-11-10 11:24:04 -0800

    Add script to show late interview/resume submissions

---
+++ scripts/submission_times.py
@@ -0,0 +1,22 @@
+import os.path, sys, time, datetime
+
+sys.path.insert( 0, '..' )
+
+from tbpsite import settings
+from django.core.management import setup_environ
+
+setup_environ( settings )
+
+from main.models import Candidate
+
+due_date = datetime.datetime(2013, 11, 8, 23, 59, 59)
+
+print "Due date is %s" % due_date
+
+due_date_seconds = time.mktime(due_date.timetuple())
+
+for c in Candidate.objects.all():
+    if c.professor_interview and os.path.getmtime(c.professor_interview.file.name) > due_date_seconds:
+        print "Late professor interview: %s (%s)" % (c, datetime.datetime.fromtimestamp(os.path.getmtime(c.professor_interview.file.name)))
+    if c.resume() and os.path.getmtime(c.resume().file.name) > due_date_seconds:
+        print "Late resume: %s (%s)" % (c, datetime.datetime.fromtimestamp(os.path.getmtime(c.resume().file.name)))
